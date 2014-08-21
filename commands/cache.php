68bdf651b9c03ea687f5a2a42c462ca14d103ca0
Stefano Azzolini <lastguest@gmail.com>
2014-08-21 18:28:02 +0200

    Shell::alias('clearCache')

--- commands/cache.php
+++ commands/cache.php
@@ -3,18 +3,22 @@
 /**
  * Cache utilities
  */
+
+Shell::alias('clearCache',function(){
+ return Shell::sequence(
+   Shell::rm('-rf', APP_DIR.'/cache/*' ),
+   Shell::mkdir( APP_DIR.'/cache/objects', APP_DIR.'/cache/views' )
+ ); 
+});
  
 CLI::on('cache :action',function($action){
   switch($action){
     case 'clear':
-        Shell::sequence(
-          Shell::rm('-rf '.APP_DIR.'/cache/*'),
-          Shell::mkdir(APP_DIR.'/cache/objects',APP_DIR.'/cache/views')
-        )->run();
+        Shell::clearCache()->run();
       break;
     default:
-      echo "Cache utilities.",PHP_EOL;
-      echo "Available actions: clear",PHP_EOL;
+        echo "Cache utilities.",PHP_EOL;
+        echo "- Available actions: clear",PHP_EOL;
       break;
   }
 });
