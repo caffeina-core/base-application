b0c289bbd8b18a503604e1a785bfa4703cced355
Stefano Azzolini <lastguest@gmail.com>
2014-09-02 23:09:04 +0200

    Update main.php

--- routes/main.php
+++ routes/main.php
@@ -1,13 +1,13 @@
 <?php
 
 Event::on(404,function(){
-  Response::html( View::from('special/error.twig',[
+  Response::html( View::from('special/error',[
     'code'    => 404,
     'message' => 'Page not found',
   ]));
 });
 
 Route::on('/',function(){
-  return View::from('index.twig');
+  return View::from('index');
 });
 
