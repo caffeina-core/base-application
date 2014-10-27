2e5c844e855580bc80848aac906a6ad681a7b71c
Stefano Azzolini <lastguest@gmail.com>
2014-10-27 16:57:57 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+{
+  "name": "Core",
+  "description": "Basic Caffeina Core Web application skeleton.",
+  "repository": "https://github.com/caffeina-core/base-application",
+  "website": "https://github.com/caffeina-core/core",
+  "logo": "https://avatars1.githubusercontent.com/u/8492338?v=2&s=200",
+  "keywords": ["php", "core", "API", "flask"]
+}
