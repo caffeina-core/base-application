1a4c7376499e2df914e8dc7727f0be27e1c3fb0d
Stefano Azzolini <lastguest@gmail.com>
2015-06-15 12:48:20 +0200

    Update common.php

--- configs/common.php
+++ configs/common.php
@@ -8,6 +8,6 @@ return [
   'debug' => false, // Disable when in production
   'cache' => [
     'directory' => null, // if null will use the sys_get_temp_dir
-    'views' 		=> true,
+    'views'     => true,
   ],
 ];
