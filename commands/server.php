4dfc353c7ec4b9ac2db9c21d534c39d5bee81bd9
Stefano Azzolini <lastguest@gmail.com>
2015-11-20 19:30:07 +0100

    Update server.php

--- commands/server.php
+++ commands/server.php
@@ -11,7 +11,7 @@ CLI::on('server :action',function($action){
         $port = CLI::input("port",8888);
         CLI::write("[<white>APP</white>] <purple>Starting</purple> webserver on <cyan>$host</cyan>:<green>$port</green>");
         CLI::writeln("");
-        Shell::php("-S","$host:$port","-t",__DIR__.'/../public')->run();
+        exec("(which open && open http://$host:$port/); php -S $host:$port -t ".dirname(__DIR__)."/public");
       break;
     default:
         echo "Server utilities.",PHP_EOL;
